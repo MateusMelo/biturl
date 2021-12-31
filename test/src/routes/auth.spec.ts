22c2633bb0d9cbe169c36088e2cb03dd7c34f888
mateusmelo <mateusmelodasilva@gmail.com>
2021-12-30 21:02:26 -0300

    test: ensure email exists

--- test/src/routes/auth.spec.ts
+++ test/src/routes/auth.spec.ts
@@ -1,13 +1,20 @@
 import request from 'supertest'
+import faker from 'faker'
 import { expect } from 'chai'
 import mongoose from 'mongoose'
 import app from './../../../src/app'
-import UserModel from './../../../src/models/User'
+import { User } from './../../../src/models/User'
 
 const userPayload = {
   name: 'test',
   email: 'test@test.com',
   password: 'test'
+}
+
+interface FakeUser {
+  name: String
+  email: String
+  password: String
 }
 
 before(async () => {
@@ -17,29 +24,39 @@ before(async () => {
 
 describe('routes/auth', () => {
   describe('sign-up', () => {
-    it('should signup user', async () => {
+    let fakeUser: FakeUser
+    beforeEach(() => {
+      fakeUser = {
+        name: faker.name.findName(),
+        email: faker.internet.email().toLowerCase(),
+        password: 'test'
+      }
+    })
+
+    it('should return 201 and signup user', async () => {
       const req = request(app)
       const res = await req.post('/auth/sign-up')
         .set('Content-Type', 'application/json')
-        .send(userPayload)
+        .send(fakeUser)
 
       expect(res.status).to.equal(201)
       expect(res.body).not.to.have.property('password')
       expect(res.body).to.have.property('id')
-      expect(res.body.name).to.equal(userPayload.name)
-      expect(res.body.email).to.equal(userPayload.email)
+      expect(res.body.name).to.equal(fakeUser.name)
+      expect(res.body.email).to.equal(fakeUser.email)
 
       if (!mongoose.Types.ObjectId.isValid(res.body.id)) throw new Error('Invalid object id')
-      const user = await UserModel.findById(res.body.id)
+      const user = await User.findById(res.body.id)
       if (user === null) throw new Error('User not found')
 
       expect(res.body.id).to.equal(user._id.toString())
-      expect(user.password).to.not.equal(userPayload.password)
+      expect(user.password).to.not.equal(fakeUser.password)
       expect(res.body.name).to.equal(user.name)
     })
   })
+
   describe('auth/sign-in', () => {
-    it('should auth with email and password', async () => {
+    it('should return 200 and auth user if email and password match', async () => {
       const req = request(app)
       const res = await req.post('/auth/sign-in')
         .set('Content-Type', 'application/json')
@@ -49,6 +66,10 @@ describe('routes/auth', () => {
         })
 
       expect(res.status).to.equal(200)
+      expect(res.body.user).to.have.property('id')
+      expect(res.body.user.name).to.equal(userPayload.name)
+      expect(res.body.user.email).to.equal(userPayload.email)
+      expect(res.body.user).not.to.have.property('password')
       expect(res.body).to.have.property('token')
       expect(res.body.token).to.be.a('string')
     })
@@ -71,6 +92,23 @@ describe('routes/auth', () => {
         .set('Content-Type', 'application/json')
         .send({
           email: userPayload.email
+        })
+
+      expect(res.status).to.equal(400)
+      expect(res.body).to.have.property('errors')
+    })
+
+    it('should return 400 if there are no users with that email', async () => {
+      const credentials = {
+        email: faker.internet.email().toLowerCase(),
+        password: 'test'
+      }
+      const req = request(app)
+      const res = await req.post('/auth/sign-in')
+        .set('Content-Type', 'application/json')
+        .send({
+          email: credentials.email,
+          password: credentials.password
         })
 
       expect(res.status).to.equal(400)
