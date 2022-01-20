d25a527e7f0a3a7ceb5ca945a63a7c0424335177
mateusmelo <mateusmelodasilva@gmail.com>
2022-01-20 16:25:43 -0300

    test: ensure url was inserted

--- test/src/routes/url.spec.ts
+++ test/src/routes/url.spec.ts
@@ -1,15 +1,16 @@
 import request from 'supertest'
 import { expect } from 'chai'
-import mongoose from 'mongoose'
+import { connect, Types } from 'mongoose'
 import app from './../../../src/app'
 import { User } from './../../../src/models/User'
 import { UserToken } from './../../../src/models/UserToken'
+import { Url } from './../../../src/models/Url'
 import { createUser, userPayload } from './../../mocks/user.mock'
 
 let authToken = ''
 describe('routes/url', () => {
   before(async () => {
-    await mongoose.connect('mongodb://localhost:27017/biturl-test')
+    await connect('mongodb://localhost:27017/biturl-test')
       .catch(err => console.log(err))
 
     await User.deleteMany({})
@@ -59,13 +60,17 @@ describe('routes/url', () => {
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${authToken}`)
         .send({
-          url: 'https://biturl/qwer'
+          url: 'https://www.xd.com/qwer'
         })
 
       expect(res.status).to.equal(201)
+      if (!Types.ObjectId.isValid(res.body.url.id)) throw new Error('Invalid object id')
+      const url = await Url.findById(res.body.url.id)
+      if (url === null) throw new Error('Url not found')
       expect(res.body).to.have.property('url')
       expect(res.body.url).to.have.property('id')
-      expect(res.body.url.url).to.equal('https://biturl/qwer')
+      expect(res.body.url.id).to.equal(url._id.toString())
+      expect(res.body.url.url).to.equal(url.url)
     })
 
     it('should return 401 when auth token is missing', async () => {
@@ -73,7 +78,7 @@ describe('routes/url', () => {
       const res = await req.post('/urls')
         .set('Content-Type', 'application/json')
         .send({
-          url: 'https://biturl/qwer'
+          url: 'https://www.xd.com/qwer'
         })
 
       expect(res.status).to.equal(401)
